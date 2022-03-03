b51d18873f1af2af4ef5f6dd21afd9aa109e02fc
Tahir Akhter <tahir.akhter@10pearls.com>
2022-03-03 12:10:55 +0500

    project files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # NSQ_Example
-NSQ with multi nodes on Docker example.
+NSQ multi nodes with Docker example.
+
+start NSQD, NSQD_LOOKUP and ADMIN Portal
+Run > docker-compose up -d
+
+Start all 3 Node Servers in different terminal to see messages distribution on console
+
+node server.js
+node server2.js
+node server3.js
+
+Admin portal to see all NSQ related details
+Admin Portal URL: http://127.0.0.1:4171/nodes
---
+++ server3.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const nsq = require('nsqjs')
+const app = express()
+const axios = require('axios');
+const port = 3003
+
+const reader = new nsq.Reader('Topic1', 'Channel1', {
+	lookupdHTTPAddresses: ['127.0.0.1:4161', '127.0.0.1:4261']
+})
+reader.connect()
+
+reader.on('error', err => {
+	console.log('Error message', JSON.stringify(err))
+})
+
+reader.on('message', msg => {
+	console.log('Received message [%s]: %s', msg.id, msg.body.toString())
+	msg.finish()
+})
+
+let count = 1;
+let msg;
+
+function sendMessage() {
+	msg = "message " + count + " from node3";
+	setTimeout(() => {
+		axios.post('http://localhost:4351/pub?topic=Topic1', { "text": msg })
+			.then(response => {
+				count = count + 1;
+				sendMessage();
+			})
+			.catch(error => {
+				sendMessage = false;
+				console.log(error);
+			});
+	}, 5000);
+
+}
+sendMessage();
+
+app.listen(port, () => console.log(`NSQ Consumer is listening on port ${port}!`))
